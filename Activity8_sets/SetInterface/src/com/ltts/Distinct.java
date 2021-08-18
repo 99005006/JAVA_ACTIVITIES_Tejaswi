b905b92bdb4bcd503892ebafbde629d229b655d7
99005006 <87120327+99005006@users.noreply.github.com>
2021-08-19 04:34:16 +0530

    Add files via upload

---
+++ Activity8_sets/SetInterface/src/com/ltts/Distinct.java
@@ -0,0 +1,21 @@
+package com.ltts;
+
+import java.util.HashSet;
+
+import java.util.Scanner;
+
+public class Distinct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		HashSet names = new HashSet();
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++) {
+			names.add(sc.nextLine());
+			
+		}
+		System.out.println(names.size());
+	}
+
+}
